8148f13dfd5d685ba7670885d8d3db45d0e4c84f
HarvesterV2 <deadshot.billa@gmail.com>
2026-04-19 17:50:52 +0200

    Pozmieniano wykresy

--- 1.R
+++ 1.R
@@ -1,7 +1,16 @@
 Zadanie1 <- function(markets)
 {
   par(mfrow=c(1,2))
-  boxplot(markets, main =paste("Mediana i Kwartyle\nMarket "),ylab="Wydatki [zł]",horizontal = FALSE,ylim=c(globalmin,globalmax))
+  boxplot(
+    markets, 
+    main = paste("Obliczona\nMediana oraz Kwartyle"),
+    xlab = "Numer marketu: ",
+    ylab="Wydatki [zł]",
+    horizontal = FALSE,
+    col = c("#11A10D", "#7F00FF"),
+    ylim=c(globalmin,globalmax)
+  )
+  grid(nx = NA, ny = NULL, col = "black", lty = "dotted")
   rysuj_odchylenie(markets)
   market1_dane = wypisz_dane(markets[[1]])
   market2_dane = wypisz_dane(markets[[2]])
@@ -15,9 +24,6 @@ wypisz_dane <- function(market) {
   market_q1 = quantile(market,prob=0.25)
   market_q3 = quantile(market,prob=0.75)
   market_IQR = market_q3-market_q1
-  #text(0.6,market_q1,"Q1",col = "blue",cex = 0.7)
-  #text(0.6,market_mediana,"Mediana",col = "red",cex = 0.7)
-  #text(0.6,market_q3,"Q3",col = "blue",cex = 0.7)
   returnValue(list(market_srednia,market_mediana,market_moda,market_q1,market_q3,market_IQR))
 }
 
@@ -35,13 +41,17 @@ rysuj_odchylenie <- function(markety) {
   
   boxplot(
     market_xx_list,
-    main = "Odchylenie standardowe\nMarket",
+    main = "Obliczone\nOdchylenie standardowe",
+    xlab = "Numer marketu: ",
     ylab = "Wydatki [zł]",
-    ylim = c(globalmin, globalmax)
-  )
+    ylim = c(globalmin, globalmax),
+    col = c("#11A10D", "#7F00FF")
+    )
+  grid(nx = NA, ny = NULL, col = "black", lty = "dotted")
 }
 
 Mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
+
