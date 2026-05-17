039fae6d578d65e900beef57c7d5f01feb0b7ebe
RaviBulwa <konrad.wydmuch54321@gmail.com>
2026-05-17 14:00:52 +0200

    Zadanie 6 dodanie 'abcd' system

--- 6.R
+++ 6.R
@@ -1,10 +1,9 @@
-Zadanie6 <- function(markets, alfa) {
-  # ponieważ sigma1 i sigma2 nie są znane i nie wiemy czy są jednakowe czy też nie, przeprowadzamy test Snedecora.
+Zadanie6 <- function(markets, alfa, rodzaj) {
   cat("=== Przeprowadzenie testu Snedecora: ===\n")
-  decyzja = test_Snedecora(markets, alfa)
-  
+  czy_wariancje_rowne = test_Snedecora(markets, alfa)
+
   cat("=== Przeprowadzenie testu średnich w marketach: ===\n")
-  test_srednie_marketow(markets, alfa, decyzja)
+  test_srednie_marketow(markets, alfa, czy_wariancje_rowne, rodzaj)
   
 }
 
@@ -44,65 +43,149 @@ test_Snedecora <- function(markets, alfa) {
   return(!decyzja)
 }
 
-test_srednie_marketow <- function(markets, alfa, czy_wariancje_rowne) {
+test_srednie_marketow <- function(markets, alfa, czy_wariancje_rowne, rodzaj) {
   
-  cat("Hipoteza zerowa - średnie w obu marketach są równe.\n")
-  cat("Hipoteza alternatywna - średnia w pierwszym markecie jest mniejsza.\n")
+  cat("Hipoteza zerowa - średnie w obu marketach są sobie równe.\n")
+  
+  if(rodzaj == "d" &&  mean( markets[[1]]) >  mean(markets[[2]])) {
+    rodzaj = "b"
+  }else {
+    rodzaj = "c"
+  }
+  
+  if (rodzaj == "a") { #obu
+    cat("Hipoteza alternatywna - średnia w obu marketach nie są sobie równe.\n")
+  } else if (rodzaj == "b") { # prawo
+    cat("Hipoteza alternatywna - średnia w pierwszym markecie jest większa.\n")
+  } else if (rodzaj == "c") { # lewo
+    cat("Hipoteza alternatywna - średnia w pierwszym markecie jest mniejsza.\n")
+  }
   
   if(czy_wariancje_rowne)
   {
     #Jeśli są jednakowe, to stosujemy model 2
-    wynik_modelu = statystyka_t_model2(markets, alfa)
+    wynik_modelu = statystyka_t_model2(markets, alfa, rodzaj)
   }
   else
   {
     #Jeśli nie są jednakowe, to stosujemy model 3
-    wynik_modelu = statystyka_C_model3(markets, alfa)
+    wynik_modelu = statystyka_C_model3(markets, alfa, rodzaj)
   }
-  statystyka = wynik_modelu$statystyka
-  df = wynik_modelu$df
-  
-  cat("Statystyka testowa:", statystyka, "\n")
-  kryt = qt(alfa, df)
-  cat("Obszar krytyczny: (-∞,", kryt, "]\n")
-  #pvalue
-  cat("P-value:", pt(statystyka, df), "\n")
-  decyzja = statystyka <= kryt
-  if (decyzja) {
-    cat("Odrzucamy H0 - wydatki w 1 markecie są mniejsze.\n")
+  cat("Statystyka testowa:", wynik_modelu$statystyka, "\n")
+  cat("Obszar krytyczny:", wynik_modelu$kryt, "\n")
+  cat("P-value:", wynik_modelu$pvalue, "\n")
+  if (wynik_modelu$decyzja) {
+    cat("Odrzucamy H0 na rzecz hipotezy alternatywnej H1.\n")
   } else {
     cat("Brak podstaw do odrzucenia H0.\n")
   }
 }
 
-statystyka_t_model2 <- function(markets, alfa) {
+statystyka_t_model2 <- function(markets, alfa, rodzaj) {
   
-  x = markets[[1]]
-  y = markets[[2]]
+  market1 = markets[[1]]
+  market2 = markets[[2]]
   
-  n1 = length(x)
-  n2 = length(y)
+  n1 = length(market1)
+  n2 = length(market2)
   
-  x1 = mean(x)
-  x2 = mean(y)
+  x1sr = mean(market1)
+  x2sr = mean(market2)
   
-  S1 = var(x)
-  S2 = var(y)
+  S1 = var(market1)
+  S2 = var(market2)
   
-  sp2 = ((n1 * S1 + n2 * S2) / (n1 + n2 - 2)) * ((n1 + n2) / (n1 * n2))
-  
-  T = (x1 - x2) / sqrt(sp2)
+  T = (x1sr - x2sr) / sqrt(((n1 * S1 + n2 * S2) / (n1 + n2 - 2)) * ((n1 + n2) / (n1 * n2)))
   
   df = n1 + n2 - 2
   
-  return(list(statystyka = T, df = df))
+  if (rodzaj == "a") {
+    
+    kryt_left = qt(alfa / 2, df)
+    kryt_right = qt(1 - alfa / 2, df)
+    
+    kryt = paste0("(-∞, ", kryt_left, "] ∪ [", kryt_right, ", ∞)")
+    
+    pvalue = 2 * (1 - pt(abs(T), df))
+    decyzja = (T <= kryt_left) || (T >= kryt_right)
+    
+  } else if (rodzaj == "b") {
+    
+    kryt = paste0("[", qt(1 - alfa, df), ", ∞)")
+    
+    pvalue = 1 - pt(T, df)
+    decyzja = (T >= qt(1 - alfa, df))
+    
+  } else if (rodzaj == "c") {
+    
+    kryt = paste0("(-∞, ", qt(alfa, df), "]")
+    
+    pvalue = pt(T, df)
+    decyzja = (T <= qt(alfa, df))
+    
+  }
+  
+  return(list(statystyka = T,kryt = kryt,pvalue = pvalue,decyzja = decyzja))
 
 }
 
-statystyka_C_model3 <- function(markets, alfa) {
+statystyka_C_model3 <- function(markets, alfa, rodzaj) {
   
-  C = (x1 - x2) / sqrt((S1 / (n1 - 1)) + (S2 / (n2 - 1)))
-  df = NA
+  market1 = markets[[1]]
+  market2 = markets[[2]]
   
-  return(list(statystyka = C, df = df))
+  n1 = length(market1)
+  n2 = length(market2)
+  
+  x1sr = mean(market1)
+  x2sr = mean(market2)
+  
+  S1 = var(market1)
+  S2 = var(market2)
+  
+  C = (x1sr - x2sr) / sqrt((S1 / (n1 - 1)) + (S2 / (n2 - 1)))
+  
+  # kwantyl Cochrana-Coxa
+  c_p = ((S1 / (n1 - 1)) * qt(alfa, n1 - 1) +
+           (S2 / (n2 - 1)) * qt(alfa, n2 - 1)) /
+    ((S1 / (n1 - 1)) + (S2 / (n2 - 1)))
+  
+  if (rodzaj == "a") {
+    
+    c_left = ((S1 / (n1 - 1)) * qt(alfa / 2, n1 - 1) +
+                (S2 / (n2 - 1)) * qt(alfa / 2, n2 - 1)) /
+      ((S1 / (n1 - 1)) + (S2 / (n2 - 1)))
+    
+    c_right = ((S1 / (n1 - 1)) * qt(1 - alfa / 2, n1 - 1) +
+                 (S2 / (n2 - 1)) * qt(1 - alfa / 2, n2 - 1)) /
+      ((S1 / (n1 - 1)) + (S2 / (n2 - 1)))
+    
+    kryt = paste0("(-∞, ", c_left, "] ∪ [", c_right, ", ∞)")
+    
+    pvalue = 2 * min(
+      pt(C, df = n1 - 1),
+      1 - pt(C, df = n1 - 1)
+    )
+    
+    decyzja = (C <= c_left) || (C >= c_right)
+    
+  } else if (rodzaj == "b") {
+    
+    kryt = paste0("[", c_p, ", ∞)")
+    
+    pvalue = 1 - pt(C, df = n1 - 1)
+    
+    decyzja = (C >= c_p)
+    
+  } else if (rodzaj == "c") {
+    
+    kryt = paste0("(-∞, ", c_p, "]")
+    
+    pvalue = pt(C, df = n1 - 1)
+    
+    decyzja = (C <= c_p)
+    
+  }
+  
+  return(list(statystyka = C,kryt = kryt,pvalue = pvalue,decyzja = decyzja))
 }
